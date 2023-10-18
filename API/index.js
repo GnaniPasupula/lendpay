a893d59c117ac03e2b1e23261d62b67f16046719
H0r0sha <gnanipasupula@gmail.com>
2023-10-18 20:23:30 +0530

    Initial commit

---
+++ API/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+function startServer() {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(cors());
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();
---
+++ Client/android/app/src/main/kotlin/com/example/lendpay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lendpay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
