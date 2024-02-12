f78a9de34b926031f3efd43f19dfd581be1bb826
GnaniPasupula <gnanipasupula@gmail.com>
2024-02-12 20:07:26 +0530

    Basic Notification Done

--- Client/lib/main.dart
+++ Client/lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lendpay/API/firebase_api.dart';
 import 'package:lendpay/Providers/activeUser_provider.dart';
 import 'package:lendpay/Providers/subTransaction_provider.dart';
 import 'package:lendpay/dashboard.dart';
+import 'package:lendpay/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './auth_screen.dart'; 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
+
   runApp(
     MultiProvider(
       providers: [
